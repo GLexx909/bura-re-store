db3195bf34664794ef526203bcfc9e65264bf458
Vitaly Simonov <guardian909@rambler.ru>
2020-05-20 17:18:20 +0300

    Отправка действий в Redux Store (action dispatch)

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -2,16 +2,31 @@ import React, { Component } from "react";
 import BookListItem from "../book-list-item";
 import { connect } from 'react-redux'
 
+import { withBookstoreService } from '../hoc'
+import { booksLoaded } from "../../actions"; // Экшен-креэйтор
+import { compose } from '../../utils'
 import './book-list.css'
 
 class BookList extends Component  {
+
+  componentDidMount() {
+    // Получение данных из сервиса (хранилища)
+    const { bookstoreService } = this.props; // оно у нас есть из-за того, что мы обернули в withBookstoreService
+    const data = bookstoreService.getBooks();
+
+    // dispatch action to store, т.е. сразу передаёт полученные данные в Redux store
+    // там внутри наш Редюсер получает команду "BOOKS_LOADED" и обновляет books
+    // обновлённый список книг снова возвращается к нашиму компоненту BookList через mapStateToProps
+    // и через render() отрисовывает данные.
+    this.props.booksLoaded(data)
+  }
 
   render() {
     const { books } = this.props;
     return (
       <ul>
         {
-          books.map((book) => {
+          books.map((book) => { // оно у нас есть из-за mapStateToProps
             return (
               <li key={book.id}><BookListItem book={book} /></li>
             )
@@ -27,5 +42,25 @@ const mapStateToProps = ({ books }) => {
   return { books }
 }
 
-// Вся эта конструкция уже будет знать о Redux store и сможет работать с ним
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = {
+  booksLoaded
+}
+
+// connect конструкция уже будет знать о Redux store и сможет работать с ним
+export default compose(
+  withBookstoreService(),
+  connect(mapStateToProps, mapDispatchToProps)
+)(BookList)
+
+// Вверху функция compose просто выполняет функцию визуального упрощения старого варианта, который приведён ниже.
+// export default withBookstoreService()(
+//   connect(mapStateToProps, mapDispatchToProps)(BookList)
+// );
+
+
+
+// Как только наш компонент загружается, происходит следующее:
+// connect оборачивает наш BookList в компонент высшего порядка, который подключается к Redux store
+// мы конфигурируем как именно происходит это подключение функциями mapStateToProps и mapDispatchToProps
+// mapStateToProps говорит какие данные наш компонент хочет получить из Redux store
+// mapDispatchToProps говорит какие действия захочет выполнить наш компонент, какие экшены он будет передавать с store.
--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,5 +1,4 @@
 import React from "react";
-import BookListItem from "../book-list-item";
 import BookList from "../book-list";
 
 const HomePage = () => {
